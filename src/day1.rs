f8f23030753d4a428b66a8faa219fbc28950ddfa
David <dbosomworth@gmail.com>
2021-02-13 15:49:59 -0600

    Cleaning up code

--- src/day1.rs
+++ src/day1.rs
@@ -5,18 +5,17 @@ fn main() {
 
     let expense_report = utility::load_file_data("./resources/day1.txt");
     
-    //go through the set, find 2020 - expense and see if it exists in the set
+    //go through the map, find 2020 - expense and see if it exists in the map
     //if it does, we found the matching pair;
 
     for expense in &expense_report {
         //expense is (key, value) where (expense value, count of expense value)
+
         let required_value = 2020 - expense.0;
 
-        if expense_report.contains_key(&required_value) 
-            && *(expense_report.get(&required_value).unwrap()) > 0 as i64 {
-
+        //if our required value exists in the map, then calculate the solution
+        if expense_report.contains_key(&required_value) {
             println!("The expected value is: {}", expense.0 * required_value);
-
             break;
         }
     }
