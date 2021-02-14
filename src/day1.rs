0257a77c86292463cc830e3e62f3a481deee2588
= <=>
2021-02-13 22:06:41 -0600

    Add part 2

--- src/day1.rs
+++ src/day1.rs
@@ -11,8 +11,10 @@ fn transform_data(values: &mut HashMap<i64, i64>, data: &i64) {
     values.entry(*data).or_insert(2020 - *data);
 }
 
+
 fn main() {
-    println!("Day 1!");
+
+    println!("Day 1: Part 1!");
 
     let expense_report = utility::load::<HashMap<i64, i64>, i64>("./resources/day1.txt", &parse_row, &transform_data);
     
@@ -26,6 +28,38 @@ fn main() {
         if expense_report.contains_key(&expense.1) {
             println!("The expected value is: {}", expense.0 * expense.1);
             break;
+        }
+    }
+
+    println!("\nDay 1: Part 2!");
+
+    
+
+    //Not a fan of the for^2.
+    //we already have the first_value, and what the remaining two values need to equal for that first_value
+    // x = first_expense
+    // 2020 - x = y + z
+    for expense in &expense_report {
+        
+        for remaining in &expense_report {
+
+            //edge case: skip ourself if we are the same
+            if expense == remaining  {
+                continue;
+            }
+
+            let missing_value = expense.1 - remaining.0;
+
+            //if we aren't really the missing value, then skip
+            if expense.0 + remaining.0 + &missing_value != 2020 {
+                continue;
+            }
+          
+            //if our required value exists in the map, then calculate the solution
+            if expense_report.contains_key(&missing_value) {
+                println!("The expected value is: {}", expense.0 * remaining.0 * &missing_value);
+                return;
+            }
         }
     }
 }
